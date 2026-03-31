c8ab56f12283d29529f79fb779293963e743ceb8
TomoProg <t.yamamoto@neogenia.co.jp>
2026-03-31 10:32:54 +0900

    pecoとnvimをインストールするように変更した

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,8 @@
 mkdir -p ~/.zsh
 
+brew install nvim
+brew install peco
+
 # gitの補完機能の追加
 curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
